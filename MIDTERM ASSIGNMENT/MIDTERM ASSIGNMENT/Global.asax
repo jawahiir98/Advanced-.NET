23a095cba6438dd6fb1544aa88e057b51a28d44e
Jawahiir Nabhan <jawahiirn@gmail.com>
2022-11-14 00:09:05 +0600

    MID TERM ASSIGNMENT

--- MIDTERM ASSIGNMENT/MIDTERM ASSIGNMENT/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MIDTERM_ASSIGNMENT.MvcApplication" Language="C#" %>
---
+++ Zero Hunger/Zero Hunger/Controllers/ZeroController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zero_Hunger.Models;
+
+namespace Zero_Hunger.Controllers
+{
+    public class ZeroController : Controller
+    {
+        // GET: Zero
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddRequest(Request R)
+        {
+            var db = new Zero_HungerEntities();
+            db.Requests.Add(R);
+            db.SaveChanges();
+            return View("Index");
+        }        
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee E)
+        {
+            var db = new Zero_HungerEntities();
+            db.Employees.Add(E);
+            db.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult ViewEmployee()
+        {
+            var db = new Zero_HungerEntities();
+            var em = db.Employees.ToList();
+            return View(em);
+        }
+       
+        public ActionResult Assign(Request R)
+        {
+            Request r = R;
+            return View(r);
+        }
+    }
+}
