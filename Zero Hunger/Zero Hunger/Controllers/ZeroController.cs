a8fe05aa5edbe35a0b19ced13fdc16f29a986dfe
Jawahiir Nabhan <jawahiirn@gmail.com>
2022-11-14 12:49:04 +0600

    Zero Hunger

--- Zero Hunger/Zero Hunger/Controllers/ZeroController.cs
+++ Zero Hunger/Zero Hunger/Controllers/ZeroController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Zero_Hunger.Models;
 
 namespace Zero_Hunger.Controllers
@@ -25,8 +26,14 @@ namespace Zero_Hunger.Controllers
             var db = new Zero_HungerEntities();
             db.Requests.Add(R);
             db.SaveChanges();
-            return View("Index");
+            return View("ViewRequest");
         }        
+        public ActionResult ViewRequest()
+        {
+            var db = new Zero_HungerEntities();
+            var R = db.Requests.ToList();
+            return View(R);
+        }
         public ActionResult AddEmployee()
         {
             return View();
@@ -45,11 +52,34 @@ namespace Zero_Hunger.Controllers
             var em = db.Employees.ToList();
             return View(em);
         }
-       
         public ActionResult Assign(Request R)
         {
-            Request r = R;
-            return View(r);
+            var db = new Zero_HungerEntities();
+            var n = db.Employees.ToList();
+            ViewBag.Id = R.Id;
+            ViewBag.Rname = R.RetaurantName;
+            ViewBag.Fname = R.FoodName;
+            ViewBag.Q = R.Quantity;
+            return View(n);
+        }
+        [HttpPost]
+        public ActionResult Assign(Collection C)
+        {
+            var db = new Zero_HungerEntities();
+            db.Collections.Add(C);
+            db.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult Collected()
+        {
+            var db = new Zero_HungerEntities();
+            var collections = db.Collections.ToList();
+            return View(collections);
+        }
+        public ActionResult RequestView(int id)
+        {
+            var db = new Zero_HungerEntities();
+            var request = (from r in db.Collections where r.RequestId == id select rb).First();
         }
     }
 }
