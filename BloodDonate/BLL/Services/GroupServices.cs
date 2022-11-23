d14e26f0b29e202166bb5016f6311153d62e5671
Jawahiir Nabhan <jawahiirn@gmail.com>
2022-11-23 12:34:59 +0600

    SOLID

---
+++ BloodDonate/BLL/Services/GroupServices.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BLL.Services
+{
+    public class GroupServices
+    {
+        public static List<GroupDTO> Get()
+        {
+            var lst = DataAccessFactory.GroupDataAccess().Get();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Group, GroupDTO>(); });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<GroupDTO>>(lst);
+            return converted;
+        }
+        public static GroupDTO Get(int id)
+        {
+            var data = DataAccessFactory.GroupDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Group, GroupDTO>(); });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<GroupDTO>(data);
+            return converted;
+        }
+        public static GroupDTO Add(GroupDTO obj)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<GroupDTO, Group>();
+                cfg.CreateMap<Group, GroupDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<Group>(obj);
+            var res = DataAccessFactory.GroupDataAccess().Add(converted);
+            return mapper.Map<GroupDTO>(res);
+        }
+    }
+}
---
+++ BloodDonate/DAL/Repos/UserRepo.cs
@@ -0,0 +1,52 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class UserRepo : IRepo<User, string, User>, IAuth
+    {
+        BloodDonateEntities db;
+        internal UserRepo()
+        {
+            db = new BloodDonateEntities();
+        }
+        public User Add(User obj)
+        {
+            db.Users.Add(obj);
+            db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(string id)
+        {
+            db.Users.Remove(db.Users.Find(id));
+            return db.SaveChanges() > 0;
+        }
+
+        public List<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        public User Get(string id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public bool Update(User obj)
+        {
+            var tmp = db.Users.Find(obj.Username);
+            db.Entry(tmp).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+        public User Authenticate(string uname,string upass)
+        {
+            return null;
+        }
+    }
+}
