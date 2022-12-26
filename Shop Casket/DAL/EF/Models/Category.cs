bf25c6e8d9552220a5a3b574699972c31d03f1f2
Jawahiir Nabhan <jawahiirn@gmail.com>
2022-12-26 11:06:45 +0600

    FP

---
+++ Shop Casket/DAL/EF/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+    }
+}
---
+++ Shop Casket/DAL/EF/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        [Required]
+        public double Total { get; set; }
+        
+    }
+}
